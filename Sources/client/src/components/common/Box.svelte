c02afce3a624192d6c844b9bfa88995ccd6311a3
Zimtir <zimtir@mail.ru>
2020-03-19 01:48:23 +0300

    add common comps

---
+++ Sources/client/src/components/common/Box.svelte
@@ -0,0 +1,15 @@
+<style>
+  .box {
+    width: 600px;
+    height: 150px;
+    border: 1px solid black;
+    display: flex;
+    align-items: center;
+    margin: 10px;
+    border-radius: 4px;
+  }
+</style>
+
+<div class="box">
+  <slot />
+</div>
---
+++ Sources/client/src/components/common/Button.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let color
+</script>
+
+<style>
+  .button {
+    height: 80px;
+    width: 150px;
+    margin: 10px;
+    border-radius: 4px;
+    border-color: dimgray;
+  }
+
+  .button.green {
+    background: darkgreen;
+    color: white;
+  }
+
+  .button.red {
+    background: red;
+    color: white;
+  }
+</style>
+
+<button class={`button ${color}`}>
+  <slot />
+</button>
